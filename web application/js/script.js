ea95951f3913a9b037ce28d789555db09b1f7746
vkidik <vladgro720@gmail.com>
2023-04-24 15:03:36 +1100

    upd

--- web application/js/script.js
+++ web application/js/script.js
@@ -1,44 +1,72 @@
+setTimeout(() => {
+  window.scrollTo(0, 0);
+}, 50);
 let x = 0;
 let y = 90;
 let inJump = false;
-function Move() {
-  document.addEventListener("keydown", function (event) {
-    if (event.keyCode == 39) {
-      x += 50;
-      runner.style.left = x + "px";
-    }
-    if (event.keyCode == 38) {
-      y += 25;
+let inPause = true;
+let i = 0;
+document.addEventListener("keydown", function (event) {
+  if (inPause) return;
+  if (event.keyCode == 39) {
+    x += 50;
+    runner.style.left = x + "px";
+  }
+  if (event.keyCode == 38) {
+    y += 25;
+    runner.style.bottom = y + "px";
+    if (y >= 115) {
+      y = 115;
       runner.style.bottom = y + "px";
-      if (y >= 115) {
-        y = 115;
-        runner.style.bottom = y + "px";
-      }
     }
-    if (event.keyCode == 40) {
-      y -= 25;
+  }
+  if (event.keyCode == 40) {
+    y -= 25;
+    runner.style.bottom = y + "px";
+    if (y <= 65) {
+      y = 65;
       runner.style.bottom = y + "px";
-      if (y <= 65) {
-        y = 65;
-        runner.style.bottom = y + "px";
-      }
     }
-    if (event.keyCode == 32) {
-      if (inJump) {
-        return;
-      }
-      inJump = true;
-      y += 30;
+  }
+  if (event.keyCode == 32) {
+    if (inJump) {
+      return;
+    }
+    inJump = true;
+    y += 30;
+    runner.style.bottom = y + "px";
+    setTimeout(() => {
+      y -= 30;
+      inJump = false;
       runner.style.bottom = y + "px";
-      setTimeout(() => {
-        y -= 30;
-        inJump = false;
-        runner.style.bottom = y + "px";
-      }, 300);
-    }
-  });
+    }, 300);
+  }
+});
+function Move(dir) {
+  if (dir == "x") {
+    console.log("по x");
+    x += 9.6;
+    runner.style.left = x + "px";
+    window.scrollBy(10, 0);
+  }
+  if (dir == "y") {
+    console.log("по y");
+  }
 }
-Move();
+Move("y");
+
+function removeStart() {
+  start.remove();
+  inPause = false;
+  window.scrollTo(0, 0);
+  function a() {
+    setTimeout(() => {
+      Move("x");
+      a();
+    }, 50);
+  }
+  a();
+}
 
 // let player = document.querySelector("#runner");
 // function windowProportions() {
